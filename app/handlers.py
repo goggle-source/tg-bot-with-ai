5cf53cc237a1cf81fcdc0c9dd658f4e7a9b5e6dc
google_source <asssv0423348@gmail.com>
2025-11-02 11:30:17 +0300

    "add logs and railway.json"

--- app/handlers.py
+++ app/handlers.py
@@ -1,9 +1,12 @@
+import logging
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from app.generate import ai_generate
+
+logging.basicConfig(level=logging.INFO, filename="logs.log", filemode="a")
 
 class Chat(StatesGroup):
     wait = State()
@@ -12,15 +15,19 @@ rout = Router()
 
 @rout.message(CommandStart())
 async def cmd_start(message: Message):
+    logging.info(f"User {message.from_user.id} started the bot")
     await message.answer("Привет! Это чат бот с искусственным интеллектом, способный ответить на все твои вопросы")
 
 @rout.message(Chat.wait)
 async def stop_message(message: Message):
+    logging.info(f"User {message.from_user.id} wait the response")
     await message.answer("ваш запрос обрабатывается, подождите")
 
 @rout.message()
 async def generating(message: Message, state: FSMContext):
+    logging.info(f"User {message.from_user.id} is generating the response")
     await state.set_state(Chat.wait)
     response = await ai_generate(message.text)
     await message.answer(response, parse_mode="Markdown")
     await state.clear()
+    logging.info(f"User {message.from_user.id} got the response")
--- run.py
+++ run.py
@@ -1,16 +1,19 @@
 import asyncio
-
+import logging
 from aiogram import Bot, Dispatcher
 from app.handlers import rout
 from config import TOKEN
 from app.handlers import rout
 
+logging.basicConfig(level=logging.INFO, filename="logs.log", filemode="a")
 
 async def main():
+    logging.info("Start polling")
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
     dp.include_router(router=rout)
     await dp.start_polling(bot)
+    logging.info("Polling stopped")
 
 if __name__ == "__main__":
     try:
